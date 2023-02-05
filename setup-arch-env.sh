c3bab49599940323706b02a67da086a6db9e7ae5
mahid <mahidsheikh@pm.me>
2023-02-04 22:51:16 -0600

    script will now request sudo permissions and added wrappedhl to backup

--- setup-arch-env.sh
+++ setup-arch-env.sh
@@ -1,19 +1,19 @@
 #! /usr/bin/bash
 
 
-if ! command -v aura &> /dev/null
+if ! command -v sudo aura &> /dev/null
 then
-    echo "Install aura you idiot sandwich: https://github.com/fosskers/aura"
+    echo "Install sudo aura you idiot sandwich: https://github.com/fosskers/aura"
     exit
 fi
 
 sudo aura -S stow
-echo "Setting up Bash Shell"
-aura -S starship # Install starship
-aura -S lsd # Install lsd, the better ls command
-aura -S bat # Install bat, the better cat command
-aura -S zoxide # Install zoxide, the better cd command
-aura -S sshpass
+echo "Setting up shell"
+sudo aura -S starship # Install starship
+sudo aura -S lsd # Install lsd, the better ls command
+sudo aura -S bat # Install bat, the better cat command
+sudo aura -S zoxide # Install zoxide, the better cd command
+sudo aura -S sshpass
 
 # Install nerd font
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/BigBlueTerminal.zip
@@ -28,7 +28,7 @@ stow bash/
 read -p "Set up Zsh? [y/n]: " zsh_opt
 if [ $zsh_opt = "y" ]
 then 
-	aura -S zsh, zsh-syntax-highlighting, zsh-autosuggestions
+	sudo aura -S zsh, zsh-syntax-highlighting, zsh-autosuggestions
 	sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 	stow zsh/
 fi
@@ -36,14 +36,14 @@ fi
 read -p "Set up browser? [y/n]: " librewolf_opt
 if [ $librewolf_opt = "y" ]
 then 
-	aura -A librewolf-bin
+	sudo aura -A librewolf-bin
 fi
 
 # Firejail
 read -p "Set up Firejail? [y/N]: " firejail_opt
 if [ $firejail_opt = "y" ] 
 then 
-	aura -S firejail
+	sudo aura -S firejail
 	stow firejail/
 fi
 
@@ -51,8 +51,8 @@ fi
 read -p "Set up Zathura? [y/N]: " zathura_opt
 if [ $zathura_opt = "y" ]
 then 
-	aura -S zathura
-	aura -S zathura-pdf-mupdf
+	sudo aura -S zathura
+	sudo aura -S zathura-pdf-mupdf
 	stow zathura/
 fi
 
@@ -60,7 +60,7 @@ fi
 read -p "Set up Neovim? [y/N]: " nvim_opt
 if [ $nvim_opt = "y" ]
 then 
-	sudo aura -S neovim
+	sudo sudo aura -S neovim
 	stow nvim/
 fi
 
@@ -69,37 +69,38 @@ read -p "Set up desktop? [y/N]: " desktop_opt
 if [ $desktop_opt = "y" ]
 then 
 	# Kitty
-	aura -S kitty
+	sudo aura -S kitty
 	stow kitty/
 
 	# Hyprland
-	aura -A hyprland
-	aura -A hyprpaper-git
-	aura -A xdg-desktop-portal-hyprland-git
+	sudo aura -A hyprland
+	sudo aura -A hyprpaper-git
+	sudo aura -A xdg-desktop-portal-hyprland-git
 	stow hypr/
+	sudo stow wrappedhl -t / 
 	
 	# Waybar
-	aura -A waybar-hyprland-git
+	sudo aura -A waybar-hyprland-git
 	stow waybar/
 	
 	# Avizo
-	aura -S brightnessctl
-	aura -S pamixer
-	aura -A avizo
+	sudo aura -S brightnessctl
+	sudo aura -S pamixer
+	sudo aura -A avizo
 	
 	# Everything else
-	aura -S dunst
-	aura -S polkit-kde-agent
-	aura -S swaylock
-	aura -S grim
-	aura -S slurp
-	aura -S wl-clipboard
-	aura -A safeeyes
+	sudo aura -S dunst
+	sudo aura -S polkit-kde-agent
+	sudo aura -S swaylock
+	sudo aura -S grim
+	sudo aura -S slurp
+	sudo aura -S wl-clipboard
+	sudo aura -A safeeyes
 fi
 
 read -p "Set up Gimp? [y/N]: " gimp_opt
 if [ $gimp_opt = "y" ]
 then 
-	aura -S gimp
+	sudo aura -S gimp
 	stow gimp
 fi
