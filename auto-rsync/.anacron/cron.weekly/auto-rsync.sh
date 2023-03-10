19c888cad94f7f24c379b1799a911870f808ba39
mahid <mahidsheikh@pm.me>
2023-03-09 22:03:37 -0600

    Updated rsync to include file excludes

--- auto-rsync/.anacron/cron.weekly/auto-rsync.sh
+++ auto-rsync/.anacron/cron.weekly/auto-rsync.sh
@@ -1,2 +1,2 @@
-#!bin/sh
-sshpass -f ~/.secrets rsync -av /home mahid@192.168.1.6::NetBackup
+#!/bin/sh
+sshpass -f ~/.secrets rsync --exclude-from=rsync-homedir-excludes.txt -aP --delete /home mahid@192.168.1.6::NetBackup
